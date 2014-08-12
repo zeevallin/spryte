c45e61d802374c49a90ad60a347c09dbc16fb655
Philip Vieira <philip@dxw.com>
2014-08-12 15:07:13 +0100

    Fix typo in requiring the routing mappers.

--- lib/spryte/routing.rb
+++ lib/spryte/routing.rb
@@ -1,7 +1,7 @@
 module Spryte
   module Routing
     DEFAULTS = { format: "json" }.freeze
-    require "spryte/routing/version"
-    require "spryte/routing/fallback"
+    require "spryte/routing/version_mapper"
+    require "spryte/routing/fallback_mapper"
   end
 end
